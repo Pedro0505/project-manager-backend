ea7cb86f98b007365afc88374a479f0526912a79
matheusg18 <matheusgabriel.18@gmail.com>
2022-03-22 22:53:10 -0300

    fix #3

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
+import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import prisma from './prisma';
-import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', async (_req: Request, res:Response) => {
-  const getAll = await prisma.user.findMany({ include: { workspaces: true } });
+app.get('/', async (_req: Request, res: Response) => {
+  const getAll = await prisma.user.findMany({
+    include: {
+      workspaces: {
+        include: { columns: { include: { cards: true } } },
+      },
+    },
+  });
 
   res.status(200).json(getAll);
 });
