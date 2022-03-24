1966d2eac683167972c63b7700fa24965a94a499
Pedro Henrique <pedrohrds05@hotmail.com>
2022-03-24 12:11:22 -0300

    fix: remove auth middleware in route register

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -2,10 +2,9 @@ import express from 'express';
 import rescue from 'express-rescue';
 import { register } from '../controllers/User';
 import { validateUserRegister } from '../middlewares';
-import auth from '../middlewares/auth';
 
 const UserRoutes = express.Router();
 
-UserRoutes.post('/register', auth, validateUserRegister, rescue(register));
+UserRoutes.post('/register', validateUserRegister, rescue(register));
 
 export default UserRoutes;
