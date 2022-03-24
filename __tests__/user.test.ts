e3488ac2282a5abeba44ad60a572f3bf76640dff
matheusg18 <matheusgabriel.18@gmail.com>
2022-03-24 16:19:49 -0300

    test: add user register email conflict test

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -8,6 +8,10 @@ import { invalidBody } from './commonTests';
 import { IUser } from '../src/interfaces/prisma';
 
 describe('Testes em /user', () => {
+  beforeAll(async () => {
+    await prisma.user.create({ data: fakeData.userRegister.requestConflictMock });
+  });
+
   afterAll(async () => {
     await prisma.user.deleteMany();
 
@@ -78,6 +82,16 @@ describe('Testes em /user', () => {
           { title: 'não foi enviado', errorMessage: 'is required', bodyOverlaod: undefined },
         ],
       });
+    });
+
+    it('quando o email já está cadastrado', async () => {
+      const { status, body } = await request(app)
+        .post('/user/register')
+        .send(fakeData.userRegister.requestConflictMock);
+
+      expect(status).toBe(409);
+      expect(body.error).toBeDefined();
+      expect(body.error.message).toMatch('email already registered');
     });
   });
 
