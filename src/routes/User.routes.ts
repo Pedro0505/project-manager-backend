c2a3e4a8c037195bae5feea1cb8ccbae3f08247d
Pedro Henrique <pedrohrds05@hotmail.com>
2022-03-24 18:59:04 -0300

    feat: login validate

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import rescue from 'express-rescue';
 import * as Controllers from '../controllers/User';
-import { validateUserRegister } from '../middlewares';
+import { validateUserLogin, validateUserRegister } from '../middlewares';
 
 const UserRoutes = express.Router();
 
 UserRoutes.post('/register', validateUserRegister, rescue(Controllers.register));
-UserRoutes.post('/login', rescue(Controllers.Login));
+UserRoutes.post('/login', validateUserLogin, rescue(Controllers.Login));
 
 export { UserRoutes };
