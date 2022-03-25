a49685d1290f3312661caa8439a4849ae8f2b552
Pedro Henrique <pedrohrds05@hotmail.com>
2022-03-25 20:27:53 -0300

    fix: getall workspace

--- src/routes/Workspace.routes.ts
+++ src/routes/Workspace.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import rescue from 'express-rescue';
 import * as Controller from '../controllers/Workspace';
+import auth from '../middlewares/auth';
 
 const WorkspaceRoutes = express.Router();
 
-WorkspaceRoutes.post('/', rescue(Controller.Create));
+WorkspaceRoutes.post('/', auth, rescue(Controller.Create));
+WorkspaceRoutes.get('/', auth, rescue(Controller.GetAll));
 
 export { WorkspaceRoutes };
