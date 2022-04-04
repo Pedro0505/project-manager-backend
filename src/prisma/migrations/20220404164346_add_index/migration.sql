8dceb3cd826306a0aa2f9a16a3506f2b418a8b20
matheusg18 <matheusgabriel.18@gmail.com>
2022-04-04 13:47:15 -0300

    feat: add index to card and column schema

---
+++ src/prisma/migrations/20220404164346_add_index/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `workspace_card` ADD COLUMN `index` INTEGER NOT NULL DEFAULT 999;
+
+-- AlterTable
+ALTER TABLE `workspace_column` ADD COLUMN `index` INTEGER NOT NULL DEFAULT 999;
