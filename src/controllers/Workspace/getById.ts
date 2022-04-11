684fe9245e69a9c061d1d153d331f1ad94e2b5ef
Matheus Santos <matheusgabriel.18@gmail.com>
2022-04-11 00:22:36 -0300

    feat: update workspace controller to new db config

--- src/controllers/Workspace/getById.ts
+++ src/controllers/Workspace/getById.ts
@@ -2,10 +2,10 @@ import { Request, Response } from 'express';
 import * as Service from '../../services/Workspace';
 
 const getById = async (req: Request, res: Response) => {
-  const id = +req.params.id;
-  const { email } = req.tokenData;
+  const { id } = req.params;
+  const { userId } = req.tokenData;
 
-  const workspace = await Service.getById(id, email);
+  const workspace = await Service.getById(id, userId);
 
   res.status(200).json({ data: workspace });
 };
