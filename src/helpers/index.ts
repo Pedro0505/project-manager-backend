13dc8cbe7b147daa5708fe594c2cdd9e2aeeb570
Matheus Santos <matheusgabriel.18@gmail.com>
2022-04-11 00:36:39 -0300

    fix: remove unused import

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,5 +3,4 @@ export * from './ConflictError';
 export * from './JwtGenerate';
 export * from './NotFoundError';
 export * from './UnauthorizedError';
-export * from './checkOwnership';
 export * from './generateUuid';
