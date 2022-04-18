31e9782662f92678fded8359d02bec6969552965
Matheus Santos <matheusgabriel.18@gmail.com>
2022-04-18 17:18:19 -0300

    test: update user body tests

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -6,7 +6,6 @@ import prisma from '../src/prisma';
 import { verifyUuid } from './utils';
 import * as fakeData from './fakeData';
 import * as seeds from './seeds';
-import { IUser } from '../src/interfaces/prisma';
 import { IUserRegister } from '../src/interfaces/routes';
 
 describe('Testes em /user', () => {
@@ -38,83 +37,154 @@ describe('Testes em /user', () => {
     });
 
     describe('quando o body é inválido', () => {
-      invalidBody<IUser, string | number>({
-        field: 'firstName',
-        baseBody: fakeData.user.register.request,
-        verb: 'post',
-        endpoint: '/user/register',
-        assertions: [
-          { title: 'como um número', errorMessage: 'must be a string', bodyOverlaod: 2 },
-          {
-            title: 'com menos de 3 caracters',
-            errorMessage: 'is at least 3 characters long',
-            bodyOverlaod: 'ab',
-          },
-          {
-            title: 'com mais de 60 caracters',
-            errorMessage: 'is up to 60 characters long',
-            bodyOverlaod: 'a'.repeat(61),
-          },
-          { title: 'não foi enviado', errorMessage: 'is required', bodyOverlaod: undefined },
-        ],
+      it('"firstName" não foi enviado', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, firstName: undefined });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"firstName" is required');
       });
 
-      invalidBody<IUser, string | number>({
-        field: 'lastName',
-        baseBody: fakeData.user.register.request,
-        verb: 'post',
-        endpoint: '/user/register',
-        assertions: [
-          { title: 'como um número', errorMessage: 'must be a string', bodyOverlaod: 2 },
-          {
-            title: 'com menos de 3 caracters',
-            errorMessage: 'is at least 3 characters long',
-            bodyOverlaod: 'ab',
-          },
-          {
-            title: 'com mais de 60 caracters',
-            errorMessage: 'is up to 60 characters long',
-            bodyOverlaod: 'a'.repeat(61),
-          },
-          { title: 'não foi enviado', errorMessage: 'is required', bodyOverlaod: undefined },
-        ],
+      it('"firstName" como um número', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, firstName: 2 });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"firstName" must be a string');
       });
 
-      invalidBody<IUser, string | number>({
-        field: 'email',
-        baseBody: fakeData.user.register.request,
-        verb: 'post',
-        endpoint: '/user/register',
-        assertions: [
-          { title: 'como um número', errorMessage: 'must be a string', bodyOverlaod: 2 },
-          {
-            title: 'é de um formato inválido',
-            errorMessage: 'must be a valid email',
-            bodyOverlaod: 'a@.co',
-          },
-          { title: 'não foi enviado', errorMessage: 'is required', bodyOverlaod: undefined },
-        ],
+      it('"firstName" com menos de 3 caracters', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, firstName: 'ab' });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"firstName" is at least 3 characters long');
       });
 
-      invalidBody<IUser, string | number>({
-        field: 'password',
-        baseBody: fakeData.user.register.request,
-        verb: 'post',
-        endpoint: '/user/register',
-        assertions: [
-          { title: 'como um número', errorMessage: 'must be a string', bodyOverlaod: 2 },
-          {
-            title: 'com menos de 6 caracters',
-            errorMessage: 'is at least 6 characters long',
-            bodyOverlaod: 'abcde',
-          },
-          {
-            title: 'com mais de 20 caracters',
-            errorMessage: 'is up to 20 characters long',
-            bodyOverlaod: 'a'.repeat(21),
-          },
-          { title: 'não foi enviado', errorMessage: 'is required', bodyOverlaod: undefined },
-        ],
+      it('"firstName" com mais de 60 caracters', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, firstName: 'a'.repeat(61) });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"firstName" is up to 60 characters long');
+      });
+
+      it('"lastName" não foi enviado', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, lastName: undefined });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"lastName" is required');
+      });
+
+      it('"lastName" como um número', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, lastName: 2 });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"lastName" must be a string');
+      });
+
+      it('"lastName" com menos de 3 caracters', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, lastName: 'ab' });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"lastName" is at least 3 characters long');
+      });
+
+      it('"lastName" com mais de 60 caracters', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, lastName: 'a'.repeat(61) });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"lastName" is up to 60 characters long');
+      });
+
+      it('"email" não foi enviado', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, email: undefined });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"email" is required');
+      });
+
+      it('"email" como um número', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, email: 2 });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"email" must be a string');
+      });
+
+      it('"email" é de um formato inválido', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, email: 'a@.co' });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"email" must be a valid email');
+      });
+
+      it('"password" não foi enviado', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, password: undefined });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"password" is required');
+      });
+
+      it('"password" como um número', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, password: 2 });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"password" must be a string');
+      });
+
+      it('"password" com menos de 6 caracters', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, password: 'abcde' });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"password" is at least 6 characters long');
+      });
+
+      it('"password" com mais de 20 caracters', async () => {
+        const { status, body } = await request(app)
+          .post('/user/register')
+          .send({ ...fakeData.user.register.request, password: 'a'.repeat(21) });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"password" is up to 20 characters long');
       });
     });
 
@@ -152,41 +222,74 @@ describe('Testes em /user', () => {
     });
 
     describe('quando o body é inválido', () => {
-      invalidBody<IUser, string | number>({
-        field: 'email',
-        baseBody: fakeData.user.login.request,
-        verb: 'post',
-        endpoint: '/user/login',
-        assertions: [
-          { title: 'como um número', errorMessage: 'must be a string', bodyOverlaod: 2 },
-          {
-            title: 'é de um formato inválido',
-            errorMessage: 'must be a valid email',
-            bodyOverlaod: 'a@.co',
-          },
-          { title: 'não foi enviado', errorMessage: 'is required', bodyOverlaod: undefined },
-        ],
+      it('"email" não foi enviado', async () => {
+        const { status, body } = await request(app)
+          .post('/user/login')
+          .send({ ...fakeData.user.login.request, email: undefined });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"email" is required');
       });
 
-      invalidBody<IUser, string | number>({
-        field: 'password',
-        baseBody: fakeData.user.login.request,
-        verb: 'post',
-        endpoint: '/user/login',
-        assertions: [
-          { title: 'como um número', errorMessage: 'must be a string', bodyOverlaod: 2 },
-          {
-            title: 'com menos de 6 caracters',
-            errorMessage: 'is at least 6 characters long',
-            bodyOverlaod: 'abcde',
-          },
-          {
-            title: 'com mais de 20 caracters',
-            errorMessage: 'is up to 20 characters long',
-            bodyOverlaod: 'a'.repeat(21),
-          },
-          { title: 'não foi enviado', errorMessage: 'is required', bodyOverlaod: undefined },
-        ],
+      it('"email" como um número', async () => {
+        const { status, body } = await request(app)
+          .post('/user/login')
+          .send({ ...fakeData.user.login.request, email: 2 });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"email" must be a string');
+      });
+
+      it('"email" é de um formato inválido', async () => {
+        const { status, body } = await request(app)
+          .post('/user/login')
+          .send({ ...fakeData.user.login.request, email: 'a@.co' });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"email" must be a valid email');
+      });
+
+      it('"password" não foi enviado', async () => {
+        const { status, body } = await request(app)
+          .post('/user/login')
+          .send({ ...fakeData.user.login.request, password: undefined });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"password" is required');
+      });
+
+      it('"password" como um número', async () => {
+        const { status, body } = await request(app)
+          .post('/user/login')
+          .send({ ...fakeData.user.login.request, password: 2 });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"password" must be a string');
+      });
+
+      it('"password" com menos de 6 caracters', async () => {
+        const { status, body } = await request(app)
+          .post('/user/login')
+          .send({ ...fakeData.user.login.request, password: 'abcde' });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"password" is at least 6 characters long');
+      });
+
+      it('"password" com mais de 20 caracters', async () => {
+        const { status, body } = await request(app)
+          .post('/user/login')
+          .send({ ...fakeData.user.login.request, password: 'a'.repeat(21) });
+
+        expect(status).toBe(400);
+        expect(body.error).toBeDefined();
+        expect(body.error.message).toMatch('"password" is up to 20 characters long');
       });
     });
 
