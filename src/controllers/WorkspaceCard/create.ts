b64d6c8bfa00c2b2c945ea841a16aff106e00de7
Matheus Santos <matheusgabriel.18@gmail.com>
2022-04-21 21:29:46 -0300

    feat: update POST /card to attribute index

--- src/controllers/WorkspaceCard/create.ts
+++ src/controllers/WorkspaceCard/create.ts
@@ -3,9 +3,9 @@ import { IWorkspaceCardCreate } from '../../interfaces/routes';
 import * as Service from '../../services/WorkspaceCard';
 
 const create = async (req: Request, res: Response) => {
-  const { content, title, columnId, index } = req.body as IWorkspaceCardCreate;
+  const { content, title, columnId } = req.body as IWorkspaceCardCreate;
 
-  const newCard = await Service.create({ content, title, columnId, index });
+  const newCard = await Service.create({ content, title, columnId });
 
   res.status(201).json({ data: newCard });
 };
--- src/services/WorkspaceCard/create.ts
+++ src/services/WorkspaceCard/create.ts
@@ -3,10 +3,16 @@ import { IWorkspaceCardCreate } from '../../interfaces/routes';
 import prisma from '../../prisma';
 
 const create = async (newcard: IWorkspaceCardCreate) => {
-  const { content, title, columnId, index } = newcard;
+  const { content, title, columnId } = newcard;
+
+  const [{ index: greaterSavedIndex }] = await prisma.workspaceCard.findMany({
+    where: { columnId },
+    orderBy: { index: 'desc' },
+    take: 1,
+  });
 
   return prisma.workspaceCard.create({
-    data: { content, title, columnId, index, id: generateUuid() },
+    data: { content, title, columnId, index: greaterSavedIndex + 1, id: generateUuid() },
   });
 };
 
