192ee807dd8dac328cb014e82fd899550387fb4d
Pedro Henrique <pedrohrds05@hotmail.com>
2022-04-26 09:25:14 -0300

    fix: create column

--- src/services/WorkspaceColumn/create.ts
+++ src/services/WorkspaceColumn/create.ts
@@ -3,9 +3,19 @@ import { IWorkspaceColumn } from '../../interfaces/routes';
 import prisma from '../../prisma';
 
 const create = async (newColumn: IWorkspaceColumn) => {
-  const { title, workspaceId, index } = newColumn;
+  const { title, workspaceId } = newColumn;
 
-  return prisma.workspaceColumn.create({ data: { title, workspaceId, index, id: generateUuid() } });
+  const result = await prisma.workspaceColumn.findMany({
+    where: { workspaceId },
+    orderBy: { index: 'desc' },
+    take: 1,
+  });
+
+  const greaterSavedIndex = result[0]?.index ?? -1;
+
+  return prisma.workspaceColumn.create({
+    data: { title, workspaceId, index: greaterSavedIndex + 1, id: generateUuid() },
+  });
 };
 
 export { create };
