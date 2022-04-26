a5aa52a6332869d9464f733974046775c61bcd7b
Pedro Henrique <pedrohrds05@hotmail.com>
2022-04-26 15:17:29 -0300

    refactor: remove unused function in ultils test

--- __tests__/utils/index.ts
+++ __tests__/utils/index.ts
@@ -1,8 +1,5 @@
-import shell from 'shelljs';
 import { validate } from 'uuid'
-
-const resetDB = async () => shell.exec('NODE_ENV=test prisma migrate reset --force --skip-seed --schema src/prisma/schema.test.prisma');
 
 const verifyUuid = (uuid: string): boolean => validate(uuid);
 
-export { resetDB, verifyUuid };
+export { verifyUuid };
