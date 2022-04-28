6ac7c5093f25855bb4f4b86ce502b7e783954d57
Pedro Henrique <pedrohrds05@hotmail.com>
2022-04-28 12:28:48 -0300

    feat: authorization to update workspacecolumn

--- src/entities/WorkspaceColumn/controllers/update.ts
+++ src/entities/WorkspaceColumn/controllers/update.ts
@@ -5,8 +5,9 @@ import * as Service from '../services';
 const update = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { title } = req.body as IWorkspaceColumnUpdate;
+  const { userId } = req.tokenData;
 
-  const result = await Service.update(id, { title });
+  const result = await Service.update(id, { title }, userId);
 
   res.status(200).json({ data: result });
 };
