bb66b5ee1f837a8c43534c44aa0ae9a418bf52ec
Pedro Henrique <pedrohrds05@hotmail.com>
2022-04-28 18:35:23 -0300

    feat: authorization to update card

--- src/entities/WorkspaceCard/controllers/update.ts
+++ src/entities/WorkspaceCard/controllers/update.ts
@@ -5,8 +5,9 @@ import * as Service from '../services';
 const update: RequestHandler = async (req, res) => {
   const { id } = req.params;
   const { columnId, content, title } = req.body as IWorkspaceCardUpdate;
+  const { userId } = req.tokenData;
 
-  const result = await Service.update(id, { columnId, content, title });
+  const result = await Service.update(id, { columnId, content, title }, userId);
 
   res.status(200).json({ data: result });
 };
