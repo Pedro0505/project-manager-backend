0d3fe6208639b30533d86f5cfec69130bd3b0582
Pedro Henrique <pedrohrds05@hotmail.com>
2022-04-28 19:29:19 -0300

    feat: add authorization validate to update many

--- src/entities/WorkspaceCard/services/updateMany.ts
+++ src/entities/WorkspaceCard/services/updateMany.ts
@@ -1,6 +1,13 @@
 import prisma from '../../../database/prisma';
+import columnAuthorization from '../../WorkspaceColumn/helper/authorization';
+import cardAuthorization from '../helper/authorization';
 
-const updateMany = async (payload: { id: string; columnId?: string }[]) => {
+const updateMany = async (payload: { id: string; columnId: string }[], userId: string) => {
+  const validationsColumn = payload.map(({ columnId }) => columnAuthorization(userId, columnId));
+  const validationsCard = payload.map(({ id }) => cardAuthorization(userId, id));
+
+  await Promise.all([...validationsCard, ...validationsColumn]);
+
   const updateOperations = payload.map(({ id, columnId }, index) =>
     prisma.workspaceCard.update({
       where: { id },
