788b8c4426d09cd81315c7471b410721336b093c
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-04 19:13:34 -0300

    feat: envs to prod

--- src/database/prisma/index.ts
+++ src/database/prisma/index.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || 'dev';
+
 const DB_URLS = {
-  development: process.env.DATABASE_URL,
+  dev: process.env.DATABASE_URL_DEV,
   test: process.env.DATABASE_URL_TEST,
+  prod: process.env.DATABASE_URL,
 };
+
 const url = DB_URLS[ENV as keyof typeof DB_URLS];
 
 const prisma = new PrismaClient({ datasources: { db: { url } } });
