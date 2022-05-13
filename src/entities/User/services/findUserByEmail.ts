5b738d99b140d25667236418508d449b10dfa0e4
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-13 16:11:09 -0300

    refactor: change response to findUserByEmail

--- src/entities/User/services/findUserByEmail.ts
+++ src/entities/User/services/findUserByEmail.ts
@@ -2,7 +2,10 @@ import NotFoundError from '../../../helpers/NotFoundError';
 import prisma from '../../../database/prisma';
 
 const findeUserByEmail = async (querry: string) => {
-  const findUser = await prisma.user.findUnique({ where: { email: querry } });
+  const findUser = await prisma.user.findUnique({
+    where: { email: querry },
+    select: { email: true, uuid: true },
+  });
 
   if (!findUser) throw new NotFoundError('User Not Found');
 
