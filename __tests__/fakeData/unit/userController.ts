57f44f62a94ade87786981a8b9244833ef36012c
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-13 14:40:04 -0300

    test: add a file fake data

---
+++ __tests__/fakeData/unit/userController.ts
@@ -0,0 +1,3 @@
+export const login = {
+  body: { email: 'pedro@gmail.com', password: '12345678' },
+}
--- __tests__/unit/controllers/user.test.ts
+++ __tests__/unit/controllers/user.test.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as Login from '../../../src/entities/User/services/login';
 import * as Controllers from '../../../src/entities/User/controllers';
+import * as fakeData from '../../fakeData/unit';
 
 describe('Users controllers', () => {
   describe('Teste Login controller', () => {
@@ -9,18 +10,18 @@ describe('Users controllers', () => {
     const res: Partial<Response> = {  };
   
     beforeEach(() => {
-      req.body = jest.fn().mockReturnValue({ email: 'pedro@gmail.com', password: '12345678' });
+      req.body = jest.fn().mockReturnValue(fakeData.userController.login.body);
       
       res.status = jest.fn().mockReturnValue(res);
       res.json = jest.fn().mockReturnValue(res);
-  
-      jest.spyOn(Login, 'login').mockResolvedValue(Promise.resolve('aRandomToken'));
+      
+      jest.spyOn(Login, 'login').mockResolvedValue('aRandomToken');
     });
-  
+    
     afterEach(() => {
       jest.restoreAllMocks();
     });
-
+    
     it('Teste se o controller responde com o status 200', async () => {
       await Controllers.login(req as Request, res as Response);
   
