ab6b8c62c3d64fb7258ffbbb06f0285f570a0c12
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-13 09:04:04 -0300

    test: add test to status response in users controller

---
+++ __tests__/unit/controllers/user.test.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import * as Login from '../../../src/entities/User/services/login';
+import * as Controllers from '../../../src/entities/User/controllers';
+
+describe('Users controllers', () => {
+  describe('Teste Login controller', () => {
+    const req: Partial<Request>= {  };
+    
+    const res: Partial<Response> = {  };
+  
+    beforeEach(() => {
+      req.body = jest.fn().mockReturnValue({ email: 'pedro@gmail.com', password: '12345678' });
+      
+      res.json = jest.fn().mockReturnValue(res);
+      res.status = jest.fn().mockReturnValue(res);
+  
+      res.json = jest.fn().mockReturnValue(res);
+      res.status = jest.fn().mockReturnValue(res);
+  
+      jest.spyOn(Login, 'login').mockResolvedValue(Promise.resolve('aRandomToken'));
+    });
+  
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('Teste se o controller responde com o status 200', async () => {
+      await Controllers.login(req as Request, res as Response);
+  
+      expect(res.status).toHaveBeenCalledWith(200);
+    });
+  });
+})
