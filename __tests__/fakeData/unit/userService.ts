25db5710d86ecc22f3aa886528a2b70ec082929e
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-14 08:37:03 -0300

    test: add test of sucess case in service register

--- __tests__/fakeData/unit/userService.ts
+++ __tests__/fakeData/unit/userService.ts
@@ -1,11 +1,11 @@
 export const login = {
   mock: {
-    email: 'pedro@gmail.com', 
-    firstName: 'Pedro', 
     id: 'a3bb213e123b2', 
+    firstName: 'Pedro', 
     lastName: 'Silva', 
+    email: 'pedro@gmail.com', 
     password: '$argon2d$v=19$m=12,t=3,p=1$bm83enlvdTU0NTAwMDAwMA$ewelLOYW/KJoVS9nu3sHnQ',
-    uuid: 'abb213e1asd' 
+    uuid: 'abb213e1asd'
   },
   serviceParams: { email: 'pedro@gmail.com', password: '12345678' },
   jwtResponse: { userId: 'a3bb213e123b2', email: 'pedro@gmail.com' }
@@ -21,4 +21,29 @@ export const loginUserWrongPassword = {
   serviceParams: { email: 'pedro@gmail.com', password: 'wrongPassword' },
   responseError: { error: { message: 'wrong password' } },
   code: 401
+};
+
+export const service = {
+  user: {
+    firstName: 'Pedro',
+    lastName: 'Silva',
+    email: 'pedro@gmail.com', 
+    password: '12345678',
+    uuid: '921-aec231d2-ea21'
+  },
+  response: {
+    id: 'a3bb213e123b2',
+    firstName: 'Pedro',
+    lastName: 'Silva',
+    email: 'pedro@gmail.com',
+    uuid: '921-aec231d2-ea21'
+  },
+  mockCreate: {
+    id: 'a3bb213e123b2',
+    email: 'pedro@gmail.com',
+    firstName: 'Pedro',
+    lastName: 'Silva',
+    password: '$argon2d$v=19$m=12,t=3,p=1$bm83enlvdTU0NTAwMDAwMA$ewelLOYW/KJoVS9nu3sHnQ',
+    uuid: '921-aec231d2-ea21'
+  }
 };
--- __tests__/unit/services/user.test.ts
+++ __tests__/unit/services/user.test.ts
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import prisma from '../../../src/database/prisma';
 import * as Service from '../../../src/entities/User/services';
 import NotFoundError from '../../../src/helpers/NotFoundError';
+import UnauthorizedError from '../../../src/helpers/UnauthorizedError';
 import * as fakeData from '../../fakeData/unit';
 import { IDecoded } from '../../interfaces/jwt';
 
@@ -10,12 +11,12 @@ describe('Teste User Service', () => {
     describe('Testando caso de sucesso do login', () => {
       beforeEach(() => {
         jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(fakeData.userService.login.mock);
-      })
-  
+      });
+
       afterEach(() => {
         jest.restoreAllMocks();
       });
-    
+
       it('Teste de caso de sucesso do login', async () => {
         const token = await Service.login(fakeData.userService.login.serviceParams);
 
@@ -25,26 +26,68 @@ describe('Teste User Service', () => {
         expect(decoded.tokenData).toStrictEqual(fakeData.userService.login.jwtResponse);
       });
     });
-    
+
     describe('Testando caso de error do login quando o usuario não é encontrado', () => {
       beforeEach(() => {
         jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
-      })
+      });
 
       afterEach(() => {
         jest.restoreAllMocks();
       });
-    
+
       it('Teste de caso de error testando a menssagem ', async () => {
         try {
-          await Service.login(fakeData.userService.loginUserNotFound.serviceParams)
+          await Service.login(fakeData.userService.loginUserNotFound.serviceParams);
         } catch (error) {
-          expect(error).toBeInstanceOf(NotFoundError)
+          expect(error).toBeInstanceOf(NotFoundError);
           if (error instanceof NotFoundError) {
-            expect(error.code).toBe(fakeData.userService.loginUserNotFound.code)
-            expect(error.message).toBe(fakeData.userService.loginUserNotFound.responseError.error.message)
+            expect(error.code).toBe(fakeData.userService.loginUserNotFound.code);
+            expect(error.message).toBe(fakeData.userService.loginUserNotFound.responseError.error.message);
           }
         }
+      });
+    });
+
+    describe('Testando caso de error do login quando o usuario não é encontrado', () => {
+      beforeEach(() => {
+        jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(fakeData.userService.login.mock);
+      });
+
+      afterEach(() => {
+        jest.restoreAllMocks();
+      });
+
+      it('Teste de caso de sucesso do login', async () => {
+        try {
+          await Service.login(fakeData.userService.loginUserWrongPassword.serviceParams);
+        } catch (error) {
+          expect(error).toBeInstanceOf(UnauthorizedError);
+          if (error instanceof UnauthorizedError) {
+            expect(error.code).toBe(fakeData.userService.loginUserWrongPassword.code);
+            expect(error.message).toBe(fakeData.userService.loginUserWrongPassword.responseError.error.message);
+          }
+        }
+      });
+    });
+  });
+  
+  describe('Testando o service de register', () => {
+    describe('Testando caso de sucesso so register', () => {
+      beforeEach(() => {
+        jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
+        jest.spyOn(prisma.user, 'create').mockResolvedValue(fakeData.userService.service.mockCreate);
+      });
+
+      afterEach(() => {
+        jest.restoreAllMocks();
+      });
+
+      it('Testando a resposta do service', async () => {
+        const register = await Service.register(fakeData.userService.service.user);
+
+        expect(() => register).not.toThrow();
+        expect(register).toStrictEqual(fakeData.userService.service.response);
       });
     });
   });
