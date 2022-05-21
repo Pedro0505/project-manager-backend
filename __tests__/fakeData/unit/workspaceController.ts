513a7ebdbb8f7e7cfef2295605fcfcb692a753e2
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-21 13:27:19 -0300

    test(unit): test to workspace controller, in getAll json to have been called with expected

--- __tests__/fakeData/unit/workspaceController.ts
+++ __tests__/fakeData/unit/workspaceController.ts
@@ -11,3 +11,19 @@ export const exclude = {
   tokenData: {  email: 'pedro@gmail.com', userId: '9e1e71ff-1643-4d4c-822a-98d5e0f609d1' },
   callService: { id: '675e28e9-2f9b-48ac-831e-4ed9a296e842', userId: '9e1e71ff-1643-4d4c-822a-98d5e0f609d1' },
 }
+
+export const getAll = {
+  mockService: [
+		{
+			id: '85e57338-db9d-4913-adbf-058b7a68d730',
+			name: 'BlogsAPI',
+			ownerId: 'e6dea152-388a-49a4-8c70-3cb73b3c97c2'
+		},
+		{
+			id: '3b6b213c-49ef-44e2-b2a3-efe9ef9504ab',
+			name: 'Store Manager',
+			ownerId: 'e6dea152-388a-49a4-8c70-3cb73b3c97c2'
+		}
+	],
+  tokenData: { email: 'pedro@gmail.com', userId: '9e1e71ff-1643-4d4c-822a-98d5e0f609d1' },
+} 
--- __tests__/unit/controllers/workspace.test.ts
+++ __tests__/unit/controllers/workspace.test.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as Create from '../../../src/entities/Workspace/services/create';
 import * as Exclude from '../../../src/entities/Workspace/services/exclude';
+import * as GetAll from '../../../src/entities/Workspace/services/getAll';
 import { IRequestToken } from '../../interfaces/express';
 import * as Controllers from '../../../src/entities/Workspace/controllers';
 import * as fakeData from '../../fakeData/unit';
@@ -85,6 +86,32 @@ describe('Testando o controller do workspace', () => {
       
       expect(Exclude.exclude).toHaveBeenCalledTimes(1);
       expect(Exclude.exclude).toHaveBeenCalledWith(fakeData.workspaceController.exclude.callService.id, fakeData.workspaceController.exclude.callService.userId);
+    });
+  });
+
+  describe('Testando o create do workspace controller', () => {
+    const req: Partial<IRequestToken>= {  };
+    
+    const res: Partial<Response> = {  };
+  
+    beforeEach(() => {
+      req.tokenData = fakeData.workspaceController.getAll.tokenData;
+
+      res.status = jest.fn().mockReturnValue(res);
+      res.json = jest.fn().mockReturnValue(res);
+
+      jest.spyOn(GetAll, 'getAll').mockResolvedValue(fakeData.workspaceController.getAll.mockService);
+    });
+  
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+  
+    it('Testando a chamada do json do getAll', async () => {
+      await Controllers.getAll(req as Request, res as Response)
+      
+      expect(res.json).toHaveBeenCalledTimes(1);
+      expect(res.json).toHaveBeenCalledWith({ data: fakeData.workspaceController.getAll.mockService });
     });
   });
 });
