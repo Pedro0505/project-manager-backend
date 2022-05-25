81a3645445ed52bb0ad45f1a0a9e1670406a3fe1
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-25 07:36:29 -0300

    test(unit): add test to sucess case in workspace service

--- __tests__/fakeData/unit/workspaceService.ts
+++ __tests__/fakeData/unit/workspaceService.ts
@@ -1,5 +1,17 @@
 export const workspaceCreate = {
   createMock: { id: '9372eef1-0acd-4899-8fd2-4ecbfdede344', name: 'Criando um novo workspace', ownerId: '36e3920b-4544-4a17-9a86-a882460960af' },
   serviceCall: { ownerId: '36e3920b-4544-4a17-9a86-a882460960af', workspaceName: 'Criando um novo workspace' },
-  serviceReturn: { id: '9372eef1-0acd-4899-8fd2-4ecbfdede344', name: 'Criando um novo workspace' }
-}
+  serviceReturn: { id: '9372eef1-0acd-4899-8fd2-4ecbfdede344', name: 'Criando um novo workspace' },
+};
+
+export const exclude = {
+  id: '6d6ef621-d0a0-44f2-aee6-5a156aeac16d',
+  userId: '3222a682-1d91-48c8-95bb-f89104dc4df5',
+};
+
+export const excludeNotFound = {
+  id: '6d6ef621-d0a0-44f2-aee6-5a156aeac16d',
+  userId: '3222a682-1d91-48c8-95bb-f89104dc4df5',
+  code: 404,
+  message: 'workspace not found',
+};
--- __tests__/unit/services/workspace.test.ts
+++ __tests__/unit/services/workspace.test.ts
@@ -1,6 +1,8 @@
-import prisma from "../../../src/database/prisma";
-import * as fakeData from "../../fakeData/unit";
+import prisma from '../../../src/database/prisma';
+import * as fakeData from '../../fakeData/unit';
 import * as Service from '../../../src/entities/Workspace/services';
+import * as workspaceAuthorization from '../../../src/entities/Workspace/helper/authorization';
+import NotFoundError from '../../../src/helpers/NotFoundError';
 
 describe('Testando o service do workspace', () => {
   describe('Testando o create do workspace', () => {
@@ -8,9 +10,9 @@ describe('Testando o service do workspace', () => {
       beforeEach(() => {
         jest.spyOn(prisma.workspace, 'create').mockResolvedValue(fakeData.workspaceService.workspaceCreate.createMock);
       });
-      
+
       afterEach(() => {
-        jest.clearAllMocks();
+        jest.restoreAllMocks();
       });
 
       it('Testando a resposta do create', async () => {
@@ -18,6 +20,26 @@ describe('Testando o service do workspace', () => {
 
         expect(() => create).not.toThrow();
         expect(create).toStrictEqual(fakeData.workspaceService.workspaceCreate.serviceReturn);
+      });
+    });
+  });
+
+  describe('Testando o exclude do workspace', () => {
+    describe('Testando caso de sucesso do exclude', () => {
+      beforeEach(() => {
+        jest.spyOn(workspaceAuthorization, 'default').mockResolvedValue(undefined);
+        jest.spyOn(prisma.workspace, 'delete').mockResolvedValue(fakeData.workspaceService.workspaceCreate.createMock);
+      });
+
+      afterEach(() => {
+        jest.restoreAllMocks();
+      });
+
+      it('Testando a resposta do service e se ele não laça erro', async () => {
+        const deleted = await Service.exclude(fakeData.workspaceService.exclude.id, fakeData.workspaceService.exclude.userId);
+
+        expect(() => deleted).not.toThrow();
+        expect(deleted).toBeUndefined();
       });
     });
   });
